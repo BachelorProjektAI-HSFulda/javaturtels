5f9675f1dbdbe570a498f0bde3df5163fca118a1
Edimo Nkoe Pierre <pedimonkoe@yahoo.com>
2017-12-07 17:01:10 +0100

    versionsmit ionic-native Camera plugins

--- BusinessContact/BusinessContact/src/app/app.module.ts
+++ BusinessContact/BusinessContact/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { MyApp } from './app.component';
 import { ContactPage } from '../pages/contact/contact';
 import { KampagnePage} from '../pages/kampagne/kampagne';
 import { TabsPage } from '../pages/tabs/tabs';
+import {voicePage } from '../pages/voice/voice';
 
 @NgModule({
   declarations: [
     MyApp,
     ContactPage,
     KampagnePage,
-    TabsPage
+    TabsPage,
+    voicePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,7 +22,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     MyApp,
     ContactPage,
     KampagnePage,
-    TabsPage
+    TabsPage,
+    voicePage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
