f1beb1f98647da9a4a945b34307089c40479596a
Edimo Nkoe Pierre <pedimonkoe@yahoo.com>
2017-12-07 17:10:15 +0100

    app mit ionic-native camera

--- BusinessContact/BusinessContact/src/app/app.module.ts
+++ BusinessContact/BusinessContact/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { MyApp } from './app.component';
 import { ContactPage } from '../pages/contact/contact';
 import { KampagnePage} from '../pages/kampagne/kampagne';
 import { TabsPage } from '../pages/tabs/tabs';
-import {voicePage } from '../pages/voice/voice';
 
 @NgModule({
   declarations: [
     MyApp,
     ContactPage,
     KampagnePage,
-    TabsPage,
-    voicePage
+    TabsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -22,8 +20,7 @@ import {voicePage } from '../pages/voice/voice';
     MyApp,
     ContactPage,
     KampagnePage,
-    TabsPage,
-    voicePage
+    TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ BusinessContact/BusinessContact/src/pages/voicerecord/voicerecord.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-voicerecord',
+  templateUrl: 'voicerecord.html'
+})
+export class VoicerecorderPage{
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
