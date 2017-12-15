120282b6fd729fe42de136d3c7df66059da5e15b
Pierre Patrice Edimo Nkoe <pierre.edimo@informatik.hs-fulda.de>
2017-12-15 16:43:23 +0100

    Fehler beseitig

--- BusinessContact/BusinessContact/src/pages/tabs/tabs.ts
+++ BusinessContact/BusinessContact/src/pages/tabs/tabs.ts
@@ -5,17 +5,17 @@ import { ContactPage } from '../contact/contact';
 import { groupsPage } from '../groups/groups';
 
 @Component({
-    templateUrl: 'tabs.html'
+  templateUrl: 'tabs.html'
 })
 export class TabsPage {
-    // this tells the tabs component which Pages
-    // should be each tab's root Page
+  // this tells the tabs component which Pages
+  // should be each tab's root Page
     tab1Root: any = ContactPage;
     tab2Root: any = KampagnePage;
     tab3Root: any = groupsPage;
 
 
-    constructor() {
+  constructor() {
 
     }
 
