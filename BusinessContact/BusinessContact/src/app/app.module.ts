ba3f0020981dce053d3a4ddc4b79a07cb645888d
Wilfrede Atonmezin Tsafack <wilfrede.atonmezin-tsafack@informatik.hs-fulda.de>
2018-01-11 00:37:41 +0100

    einige Anpassungen SettingsSeite

--- BusinessContact/BusinessContact/src/app/app.module.ts
+++ BusinessContact/BusinessContact/src/app/app.module.ts
@@ -24,6 +24,8 @@ import { camerSeitePage } from '../pages/camerSeite/camerSeite';
 import { imgWahlPage } from '../pages/imgWahl/imgWahl';
 import { audioPage } from '../pages/audioPage/audioPage';
 import { Media } from '@ionic-native/media';
+import { synchronisationPage } from '../pages/synchronisation/synchronisation';
+import { accountInformationPage } from '../pages/accountInformation/accountInformation';
 
 
 @NgModule({
@@ -48,7 +50,10 @@ import { Media } from '@ionic-native/media';
         camerSeitePage,
         imgWahlPage,
         contactsOfKampagnePage,
-        audioPage
+        audioPage, 
+        synchronisationPage,
+        accountInformationPage
+
       
     ],
     imports: [
@@ -76,7 +81,9 @@ import { Media } from '@ionic-native/media';
         camerSeitePage,
         imgWahlPage,
         contactsOfKampagnePage,
-        audioPage
+        audioPage,
+        synchronisationPage,
+        accountInformationPage
       
     ],
     providers: [
---
+++ BusinessContact/BusinessContact/src/pages/accountInformation/accountInformation.ts
@@ -0,0 +1,22 @@
+﻿import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the accountInformation page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+    selector: 'page-accountInformation',
+    templateUrl: 'accountInformation.html'
+})
+export class accountInformationPage {
+
+    constructor(public navCtrl: NavController, public navParams: NavParams) { }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad accountInformationPage');
+    }
+
+}
