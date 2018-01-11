a47741013c1980bf28ef8f77ff0bfe1e9665dcde
Mwande <dennis.mwangi@informatik.hs-fulda.de>
2018-01-11 20:23:36 +0100

    Einige Grundlegende Änderungen am Appverhalten nach Kundengespräch - Menü / Hinzufügen von Items

--- BusinessContact/BusinessContact/src/app/app.component.ts
+++ BusinessContact/BusinessContact/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { helpPage } from '../pages/help/help'; 
 import { settingsPage}from'../pages/settings/settings'; 
 import { StatusBar, Splashscreen } from 'ionic-native';
+import { MenuController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'app.html'
@@ -14,7 +15,7 @@ export class MyApp {
     rootPage: any = TabsPage;
     @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform) {
+    constructor(platform: Platform, public menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -23,29 +24,39 @@ export class MyApp {
     });
     }
 
+  closeSideMenu() {
+      this.menuCtrl.close();
+  }
 
   signOut()
   {
-      this.nav.setRoot(anmeldungPage); 
+      this.nav.setRoot(anmeldungPage);
+      this.closeSideMenu();
   }
 
   goToprofil()
   {
       this.nav.push(profilePage);
+      this.closeSideMenu();
   }
 
   gotoHelp()
   {
       this.nav.push(helpPage);
+      this.closeSideMenu();
   }
 
   goToShare()
   {
-      alert("Share Contakt or Campaign"); 
+      if (confirm("Share Contact or Campaign?")==true) {
+          alert("sharing...")
+          this.closeSideMenu();
+      }
   }
 
   goToSettings()
   {
-      this.nav.push(settingsPage); 
+      this.nav.push(settingsPage);
+      this.closeSideMenu();
   }
 }
