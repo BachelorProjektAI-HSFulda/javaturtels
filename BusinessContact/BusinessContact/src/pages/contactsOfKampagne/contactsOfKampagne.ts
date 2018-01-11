c2acaefcebc7a68de72eeba0706dd8eb609f6999
mwande <dennis.mwangi@informatik.hs-fulda.de>
2018-01-11 15:11:19 +0100

    merge

--- BusinessContact/BusinessContact/src/pages/contactsOfKampagne/contactsOfKampagne.ts
+++ BusinessContact/BusinessContact/src/pages/contactsOfKampagne/contactsOfKampagne.ts
@@ -57,12 +57,10 @@ export class contactsOfKampagnePage {
     }
 
     pushPage() {
-         this.navCtrl.push(ReaderPage, {
-           img: this.srcImage,
-           callback: this.presentActionSheet
-          
-        }
-
+        this.navCtrl.push(ReaderPage, {
+            img: this.srcImage,
+            callback: this.presentActionSheet
+        })
     }
 
     getPicture(sourceType: number) {
